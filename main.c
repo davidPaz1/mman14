e1a69eabfc1efd9218573777ab17020664f83bc6
david paz <13david.paz@gmail.com>
2025-05-29 15:44:51 +0300

    ignore

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "define.h"
 
 int main(int argc, char *argv[]){
-    int input[] = {0,0,0,1,1,0,1,1,2,9};
+    int input[BASE2_INSTRUCTION_LENGTH] = {0,0,0,1,1,0,1,1,2,9};
     char *base4_output;
     base4_output = base4(input);
 
