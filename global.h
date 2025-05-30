7bc03d9c5af1ee3c30459fce07dbeb162704c93c
david paz <13david.paz@gmail.com>
2025-05-30 13:29:14 +0300

    spent the day to learn what are enums and decided to use them + opcode

--- global.h
+++ global.h
@@ -5,18 +5,50 @@
 #include <string.h>
 #include <stdlib.h>
 /* defines */
+#define NULL_TERMINATOR 1
+
 #define MAX_INFILE_LENGTH 81 /* +1 for null terminator */
 
 #define BASE2_INSTRUCTION_LENGTH 10
 #define BASE4_INSTRUCTION_LENGTH 5
-#define NULL_TERMINATOR 1
+
+#define REGISTER_SIZE 10
+
+#define NULL_INITIAL 2 /* used to "forget" previous errorCode */
 
 /* error codes */
-#define NULL_INITIAL 2 /*used to "forget" previous errorCode*/
-#define EOF_REACHED 3
-#define MALLOC_ERROR 4
-#define FILE_READ_ERROR 5
-#define FILE_WRITE_ERROR 6
+typedef enum ErrorCode {
+    EOF_REACHED = 3,
+    MALLOC_ERROR = 4,
+    FILE_READ_ERROR = 5,
+    FILE_WRITE_ERROR = 6
+} ErrorCode;
+
+typedef enum opCode {
+    mov = 0,
+    cmp,
+    add,
+    sub,
+    not,
+    clr,
+    lea,
+    inc,
+    dec,
+    jmp,
+    bne,
+    red,
+    prn,
+    jsr,
+    rts,
+    stop,
+    invalid = -1
+} opCode;
+
+/*enums*/
+typedef enum boolean {
+    FALSE = 0,
+    TRUE = 1
+} Bool;
 
 
 /* functions */
