4cdef5ca81ae0d36e41eb5710a420cd738f3a767
david paz <13david.paz@gmail.com>
2025-06-16 23:47:22 +0300

    fogor

--- macroTable.c
+++ macroTable.c
@@ -147,7 +147,7 @@ macroBody* findMacro(macroTable* table, char* macroName) {
 
 Bool isMacroNameValid(char* macroName) {
     int i; /* index for iterating through the macro name */
-    if (strlen(macroName) == 0 || strlen(macroName) > MAX_MACRO_SIZE) { /*test123*/
+    if (strlen(macroName) == 0 || strlen(macroName) > MAX_MACRO_SIZE) {
         fprintf(stderr, "Macro name must be between 1 and 30 characters long\n");
         return FALSE; /* exit if the macro name is not valid */
     }
