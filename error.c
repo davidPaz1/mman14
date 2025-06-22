2eb30c4fbf13fe20985c0996d5983fd8069157fe
david paz <13david.paz@gmail.com>
2025-06-22 22:40:46 +0300

    added new error codes and messages

--- error.c
+++ error.c
@@ -18,7 +18,7 @@ char* getErrorMessage(ErrCode code) {
         case UNEXPECTED_NULL_INPUT: /* 7 */
             return "unexpected NULL input, should never be used.";
 
-        /* scan errors 10 - 19 */
+        /* scan errors 10 - 29 */
         case LINE_TOO_LONG: /* 13 */
             return "line length in file is longer than allowed (80).";
         case FILE_READ_ERROR: /* 14 */
@@ -29,6 +29,16 @@ char* getErrorMessage(ErrCode code) {
             return "file mode error, should never be used.";
         case FILE_DELETE_ERROR: /* 17 */
             return "file delete error.";
+        case LABEL_INVALID_START_CHAR: /* 18 */
+            return "label starts with an invalid character, should start with a letter.";
+        case LABEL_INVALID_CHAR: /* 19 */
+            return "label contains invalid characters, should only contain letters and digits.";
+        case LABEL_TOO_LONG: /* 21 */
+            return "label is too long, should be less than 30 characters.";
+        case LABEL_EMPTY: /* 22 */
+            return "label does not have a name, should not be empty.";
+        case LABEL_TEXT_AFTER_COLON: /* 23 */
+            return "text found after label colon, should not have any text after the colon.";
 
         /* macroTable errors 30 - 39 */
         case MACRO_NAME_EXISTS: /* 31 */
@@ -39,13 +49,22 @@ char* getErrorMessage(ErrCode code) {
             return "macro name is too long.";
         case MACRO_NAME_EMPTY: /* 34 */
             return "macro name is empty.";
-        case MACRO_NAME_INVALID_CHAR: /* 35 */
+        case MACRO_NAME_INVALID_START_CHAR: /* 35 */
+            return "macro name starts with an invalid character, should start with a letter.";
+        case MACRO_NAME_INVALID_CHAR: /* 36 */
             return "macro name contains invalid characters.";
-        case MACRO_NAME_KEYWORD: /* 36 */
-            return "macro name is a set keyword by the assembley language.";
+        case MACRO_NAME_KEYWORD: /* 37 */   
+            return "macro name is a set keyword by the assembly language.";
 
-        /* firstPass errors 40 - 49 */
-        case FIRSTPASS_FAILURE: /* 41 */
+        /* symbolTable errors 40 - 49 */
+        case SYMBOLTABLE_SUCCESS: /* 40 */
+            return "symbol table operation was successful.";
+        case SYMBOL_NAME_EXISTS: /* 41 */
+            return "symbol name already exists.";
+
+        
+        /* firstPass errors 60 - 69 */
+        case FIRSTPASS_FAILURE: /* 61 */
             return "first pass failed.";
 
         /* it should never reach here */
--- error.h
+++ error.h
@@ -17,7 +17,7 @@ typedef enum ErrCode {
     UNKNOWN_ERROR = 6, /* unknown error, should never be used */
     UNEXPECTED_NULL_INPUT = 7, /* unexpected NULL input, should never be used */
 
-    /* scan errors 10 - 19 */
+    /* scan errors 10 - 29 */
     SCAN_SUCCESS = 10,
     END_OF_LINE = 11,
     TOKEN_IS_LABEL = 12, /* label found in the line */
@@ -26,9 +26,11 @@ typedef enum ErrCode {
     FILE_WRITE_ERROR = 15,
     INVALID_FILE_MODE = 16, /* invalid file mode, should never be used */
     FILE_DELETE_ERROR = 17, /* file delete error */
-
-    /* preprocessor errors 20 - 29 */
-    PREPROCESSOR_SUCCESS = 20, /* preprocessor executed successfully */
+    LABEL_INVALID_START_CHAR = 18, /* invalid start character */
+    LABEL_INVALID_CHAR = 19, /* invalid character in the line */
+    LABEL_TOO_LONG = 21, /* label is too long */
+    LABEL_EMPTY = 22, /* label is empty */
+    LABEL_TEXT_AFTER_COLON = 23, /* text found after label colon */
 
     /* macroTable errors 30 - 39 */
     MACROTABLE_SUCCESS = 30, /* macro operation was successful */
@@ -36,12 +38,20 @@ typedef enum ErrCode {
     UNMATCHED_MACRO_END = 32, /* macro not found */
     MACRO_NAME_TOO_LONG = 33, /* macro name is too long */
     MACRO_NAME_EMPTY = 34, /* macro name is empty */
-    MACRO_NAME_INVALID_CHAR = 35, /* macro name contains invalid characters */
-    MACRO_NAME_KEYWORD = 36, /* macro name is a keyword, should never be used */
+    MACRO_NAME_INVALID_START_CHAR = 35, /* macro name starts with an invalid character */
+    MACRO_NAME_INVALID_CHAR = 36, /* macro name contains invalid characters */
+    MACRO_NAME_KEYWORD = 37, /* macro name is a keyword, should never be used */
 
-    /* firstPass errors 40 - 49 */
-    FIRSTPASS_SUCCESS = 40, /* first pass was successful */
-    FIRSTPASS_FAILURE = 41 /* first pass error, should never be used */
+    /* symbolTable errors 40 - 49 */
+    SYMBOLTABLE_SUCCESS = 40, /* symbol table operation was successful */
+    SYMBOL_NAME_EXISTS = 41, /* symbol name already exists */
+
+    /* preprocessor errors 50 - 59 */
+    PREPROCESSOR_SUCCESS = 50, /* preprocessor executed successfully */
+
+    /* firstPass errors 60 - 69 */
+    FIRSTPASS_SUCCESS = 60, /* first pass was successful */
+    FIRSTPASS_FAILURE = 61 /* first pass error, should never be used */
 
 } ErrCode;
 
